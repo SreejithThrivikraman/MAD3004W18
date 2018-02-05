765d753da451db323bdee2d6c7ab3911ce4398fb
Sreejith Thrivikraman <mail.sreejith.23@gmail.com>
2018-02-05 00:45:05 -0500

    Add files via upload

---
+++ Day 3/Task_2.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var person = [String: Any]()
+let address = [String : String]()
+person["firstName"] = "Sreejith"
+person["lastName"] = "Thrivikraman"
+person["age"] = Int(50)
+person["address"] = ["street" : "265 Yorkland Blvd", "area" : "North York", "postalCode" : "M1H1Y1"]
+person["total amount"] = Double(2000)
+print("person: ",person)
+
+
